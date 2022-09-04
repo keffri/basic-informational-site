4a228d0688592cdde47c30b9e0638705ccd4e890
keffri <keffri.w.neal@gmail.com>
2022-09-04 12:51:05 -0700

    serving functionality complete

--- index.js
+++ index.js
@@ -2,16 +2,46 @@ const http = require("http");
 const fs = require("fs");
 const path = require("path");
 
-const PORT = process.env.PORT || 8080;
-
 const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    fs.readFile(path.join(__dirname, "index.html"), (err, content) => {
-      if (err) throw err;
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.end(content);
-    });
+  const filePath = path.join(
+    __dirname,
+    req.url === "/" ? "index.html" : req.url
+  );
+
+  let extensionName = path.extname(filePath);
+
+  let contentType = "text/html";
+
+  switch (extensionName) {
+    case ".js":
+      contentType = "text/javascript";
+      break;
+    case ".css":
+      contentType = "text/css";
+      break;
   }
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      if (err.code === "ENOENT") {
+        // Page not found
+        fs.readFile(path.join(__dirname, "404.html"), (err, content) => {
+          res.writeHead(200, { "Content-Type": "text/html" });
+          res.end(content, "utf8");
+        });
+      } else {
+        // Server error
+        res.writeHead(500);
+        res.end(`Server Error: ${err.code}`);
+      }
+    } else {
+      // Success
+      res.writeHead(200, { "Content-Type": contentType });
+      res.end(content, "utf8");
+    }
+  });
 });
+
+const PORT = process.env.PORT || 8080;
 
 server.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
