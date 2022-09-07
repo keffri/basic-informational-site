a24b21fa5376d09d8430e99bea1575734a05f19c
keffri <keffri.w.neal@gmail.com>
2022-09-07 14:37:27 -0700

    rewritten with Express

--- index.js
+++ index.js
@@ -1,47 +1,25 @@
-const http = require("http");
-const fs = require("fs");
+const express = require("express");
+const app = express();
 const path = require("path");
 
-const server = http.createServer((req, res) => {
-  const filePath = path.join(
-    __dirname,
-    req.url === "/" ? "index.html" : req.url
-  );
+const port = 8080;
 
-  let extensionName = path.extname(filePath);
-
-  let contentType = "text/html";
-
-  switch (extensionName) {
-    case ".js":
-      contentType = "text/javascript";
-      break;
-    case ".css":
-      contentType = "text/css";
-      break;
-  }
-
-  fs.readFile(filePath, (err, content) => {
-    if (err) {
-      if (err.code === "ENOENT") {
-        // Page not found
-        fs.readFile(path.join(__dirname, "404.html"), (err, content) => {
-          res.writeHead(200, { "Content-Type": "text/html" });
-          res.end(content, "utf8");
-        });
-      } else {
-        // Server error
-        res.writeHead(500);
-        res.end(`Server Error: ${err.code}`);
-      }
-    } else {
-      // Success
-      res.writeHead(200, { "Content-Type": contentType });
-      res.end(content, "utf8");
-    }
-  });
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
-const PORT = process.env.PORT || 8080;
+app.get("/about.html", (req, res) => {
+  res.sendFile(path.join(__dirname, "about.html"));
+});
 
-server.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+app.get("/contact.html", (req, res) => {
+  res.sendFile(path.join(__dirname, "contact.html"));
+});
+
+app.get("/404.html", (req, res) => {
+  res.sendFile(path.join(__dirname, "404.html"));
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port: ${port}.`);
+});
