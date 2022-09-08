601d3479980a6ddeade52b9e018f7d4289a5bc23
keffri <keffri.w.neal@gmail.com>
2022-09-08 05:04:52 -0700

    created public folder + serving static files

--- index.js
+++ index.js
@@ -4,21 +4,7 @@ const path = require("path");
 
 const port = 8080;
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
-
-app.get("/about.html", (req, res) => {
-  res.sendFile(path.join(__dirname, "about.html"));
-});
-
-app.get("/contact.html", (req, res) => {
-  res.sendFile(path.join(__dirname, "contact.html"));
-});
-
-app.get("/404.html", (req, res) => {
-  res.sendFile(path.join(__dirname, "404.html"));
-});
+app.use(express.static("public"));
 
 app.listen(port, () => {
   console.log(`Server running on port: ${port}.`);
